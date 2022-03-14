ef4c84f2172f147d0f25ff6c979a21dfea172983
disha080309 <84317794+disha080309@users.noreply.github.com>
2022-03-14 15:38:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,11 +20,7 @@ function gotResult(error, results) {
         random_number_r = Math.floor(Math.random() * 255) + 1;
         random_number_g = Math.floor(Math.random() * 255) + 1;
         random_number_b = Math.floor(Math.random() * 255) + 1;
-
-        console.log("Red "+random_number_r);
-        console.log("Green "+random_number_g);
-        console.log("Blue "+random_number_b);
-
+    
         document.getElementById("detected").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
         document.getElementById("detected").style.fontFamily = 'Courier New'+","+'Courier'+","+'monospace';
 
@@ -34,25 +30,21 @@ function gotResult(error, results) {
 
         img = document.getElementById("image");
 
-        if(results[0].label == "Barking"){
+        if(results[0].label == "dog"){
             img.src = "https://post.healthline.com/wp-content/uploads/2020/08/3180-Pug_green_grass-732x549-thumbnail-732x549.jpg";
-            dog = dog+1;
-            document.getElementById("detected").innerHTML = "Detected Dog - "+ dog;
+            document.getElementById("detected").innerHTML = "Detected Dog - "+Math.floor(results[0].confidence*100) + "%";
         }
-        else if(results[0].label == "Meowing"){
+        else if(results[0].label == "cat"){
             img.src = "https://www.thesprucepets.com/thmb/QDw4vt7XXQejL2IRztKeRLow6hA=/2776x1561/smart/filters:no_upscale()/cat-talk-eyes-553942-hero-df606397b6ff47b19f3ab98589c3e2ce.jpg";
-            cat = cat+1;
-            document.getElementById("detected").innerHTML = "Detected Cat - "+ cat;
+            document.getElementById("detected").innerHTML = "Detected Cat - "+ Math.floor(results[0].confidence*100) + "%";
         }
-        else if(results[0].label == "Roar"){
+        else if(results[0].label == "tiger"){
             img.src = "https://files.worldwildlife.org/wwfcmsprod/images/Tiger_resting_Bandhavgarh_National_Park_India/hero_small/6aofsvaglm_Medium_WW226365.jpg";
-            lion = lion+1;
-            document.getElementById("detected").innerHTML = "Detected Lion - "+ lion;
+            document.getElementById("detected").innerHTML = "Detected Lion - "+ Math.floor(results[0].confidence*100) + "%";
         }
         else{
             img.src = "https://shravaripatil.github.io/Sound_controlled_animals/listen.gif";
-            background_noise = background_noise+1;
-            document.getElementById("detected").innerHTML = "Detected Background Noise - "+ background_noise;
+            document.getElementById("detected").innerHTML = "Detected Background Noise - "+ Math.floor(results[0].confidence*100) + "%";
         }
     }
 }
